2ecffac3afd3575f7f78e2909b88455b3ad1f5ac
Eric Chang <echang15@gmail.com>
2017-08-21 15:52:01 -0400

    fixed models

--- myproject/todos/models.py
+++ myproject/todos/models.py
@@ -10,5 +10,5 @@ class Todo(models.Model):
     due_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
-    """this sets the default return for this object"""
-    return self.description
+        """this sets the default return for this object"""
+        return self.description
