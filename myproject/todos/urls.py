1aa77a7ed03d07636b0bfd3fb723c2b6534d682c
JP White <jpwhite3@gmail.com>
2019-07-08 21:06:13 -0400

    updating to bootstrap4 and django 2.2.3

--- myproject/todos/urls.py
+++ myproject/todos/urls.py
@@ -1,11 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^todos/$', views.todo_list.as_view(), name='todo_list'),
-    url(r'^todo/create/$', views.todo_create.as_view(), name='todo_create'),
-    url(r'^todo/(?P<pk>\d+)/$', views.todo_details.as_view(), name='todo_details'),
-    url(r'^todo/(?P<pk>\d+)/update/$', views.todo_update.as_view(), name='todo_update'),
-    url(r'^todo/(?P<pk>\d+)/delete/$', views.todo_delete.as_view(), name='todo_delete'),
+    path('', views.index, name='index'),
+    path('todos/', views.todo_list.as_view(), name='todo_list'),
+    path('todo/create/', views.todo_create.as_view(), name='todo_create')
 ]
--- myproject/todos/views.py
+++ myproject/todos/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
-from django.core.urlresolvers import reverse
-from django.urls import reverse_lazy
-from django.views.generic.detail import DetailView
+from django.urls import reverse
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.db.models import Count
 
-
+from django.contrib.auth.models import User
 from todos.models import Todo
-# Create your views here.
 
 
 def index(request):
-    todo_count = Todo.objects.all().count()
-    return render(request, "todos/index.html",{'todo_count': todo_count})
+    todos_by_user = User.objects.annotate(num_todos=Count('todo'))
+    context = {
+        'todos_by_user': todos_by_user
+    }
+    return render(request, "todos/index.html", context)
 
 
 class todo_list(ListView):
@@ -22,33 +23,11 @@ class todo_list(ListView):
     model = Todo
 
 
-class todo_details(DetailView):
-    ''' This will display a page with the details of a single todo '''
-    model = Todo
-
-
-@method_decorator(login_required, name='dispatch')
+@method_decorator(login_required, name='dispatch')  # Require login
 class todo_create(CreateView):
     ''' This will display a simple form and allow users to create a todo '''
     model = Todo
     fields = ['user', 'description', 'due_date']
 
     def get_success_url(self):
-        return reverse('todo_details', kwargs={'pk': self.object.pk})
-
-
-@method_decorator(login_required, name='dispatch')
-class todo_update(UpdateView):
-    ''' update a todo, then redirect back to its details page '''
-    model = Todo
-    fields = ['user', 'description', 'due_date']
-
-    def get_success_url(self):
-        return reverse('todo_details', kwargs={'pk': self.object.pk})
-
-
-@method_decorator(login_required, name='dispatch')
-class todo_delete(DeleteView):
-    ''' Delete a specific todo (with confirmation page), and redirect back to list view '''
-    model = Todo
-    success_url = reverse_lazy('todo_list')
+        return reverse('todo_list')
